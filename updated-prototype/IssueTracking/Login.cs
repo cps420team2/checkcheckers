0c5e35f97fc8e50731955f5f6dd537a397e4c64c
Dominic-team2 <djp.bju@gmail.com>
2017-12-01 16:34:05 -0500

    Added the first of the checks. the login causes explosions.

--- updated-prototype/IssueTracking/Login.cs
+++ updated-prototype/IssueTracking/Login.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+using System.Net.Http;
 
 namespace Prototype
 {
     public partial class Login : Form
     {
+
+        private static readonly HttpClient client = new HttpClient();
         int permissions;
 
         public Login(int level)
@@ -21,50 +23,29 @@ namespace Prototype
             InitializeComponent();
         }
 
+        public static async Task GetLoginAsync(string user, string pass)
+        {
+            var values = new Dictionary<string, string>
+            {
+               { "username", user },
+               { "password", pass }
+            };
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync(requestUri("file://C:/users/Admin-00/Desktop/Sync/checkcheckers/api/app/models/user.js"), content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+        }
+
+        private static string requestUri(string v)
+        {
+            throw new NotImplementedException();
+        }
+
         private void On_OK_Click(object sender, EventArgs e)
         {
-            /*
-            string dbconnect = "Server=localhost;Database=ccproject;Uid=root;Pwd=Password;";
-            MySqlConnection connection = new MySqlConnection(dbconnect);
-            using (connection)
-            {
-                connection.Open();
-                try
-                {
-                    string quer = "Select * from users where LoginName = @uname and Password = @pass";
-                    MySqlCommand cmd = new MySqlCommand(quer, connection);
-                    cmd.Parameters.AddWithValue("@pass", passText.Text);
-                    cmd.Parameters.AddWithValue("@uname", unameText.Text);
-                    MySqlDataReader read = cmd.ExecuteReader();
-                    if (cmd == null)
-                    {
-                        MessageBox.Show("Login Information Invalid. Please try again.");
-                        connection.Close();
-                    }
-                    else
-                    {
-                        MySqlDataAdapter cData = new MySqlDataAdapter(cmd);
-                        DataSet cDS = new DataSet();
-                        cData.Fill(cDS);
-                        if (read.GetChar("Role") == 'M' || read.GetChar("Role") == 'A') { permissions = 1; }
-                        else if (read.GetChar("Role") == 'S') { permissions = 2; }
-                        else { permissions = 0; }
-                    }
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("An Error has occured!\nPlease try again in a short time.\nIf the problem persists, contact your Manager or System Administrator.\n\n");
-                    this.Hide();
-                    var llog = new IssueTracking.MainForm();
-                    llog.FormClosed += (s, args) => this.Close();
-                    llog.Show();
-                }
-                finally
-                {
-                    connection.Close();
-                }
-            }
-            */
+            GetLoginAsync(unameText.Text, passText.Text).Wait();
+      
             //show next window, pass permissions as level
             //idea modified from https://stackoverflow.com/questions/5548746/c-sharp-open-a-new-form-then-close-the-current-form
             this.Hide();
