47c4eae5303fc93dcb144d8d58c169def4165617
Dominic Palermo <32023175+Dominic-team2@users.noreply.github.com>
2017-12-02 14:30:48 -0600

    Broken Commit: Do Not Use! Updated the progam a bit. It complains about a not-implemented error, even though it is fully implemented. Researching into the problem.

--- updated-prototype/IssueTracking/Login.cs
+++ updated-prototype/IssueTracking/Login.cs
@@ -11,8 +11,29 @@ using System.Net.Http;
 
 namespace Prototype
 {
+
     public partial class Login : Form
     {
+        private static async Task<bool> GetLoginAsync(string user, string pass)
+        {
+            var values = new Dictionary<string, string>
+            {
+               { "username", user },
+               { "password", pass }
+            };
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync(requestUri("file:\\\\\\C:\\Users\\djpbj\\Desktop\\School\\420\\FinalProject\\checkcheckers\api\app\\models\\user.js"), content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return false;
+        }
+
+        private static string requestUri(string v)
+        {
+            throw new NotImplementedException();
+        }
 
         private static readonly HttpClient client = new HttpClient();
         int permissions;
@@ -23,28 +44,13 @@ namespace Prototype
             InitializeComponent();
         }
 
-        public static async Task GetLoginAsync(string user, string pass)
-        {
-            var values = new Dictionary<string, string>
-            {
-               { "username", user },
-               { "password", pass }
-            };
-            var content = new FormUrlEncodedContent(values);
-
-            var response = await client.PostAsync(requestUri("file://C:/users/Admin-00/Desktop/Sync/checkcheckers/api/app/models/user.js"), content);
-
-            var responseString = await response.Content.ReadAsStringAsync();
-        }
-
-        private static string requestUri(string v)
-        {
-            throw new NotImplementedException();
-        }
-
         private void On_OK_Click(object sender, EventArgs e)
         {
-            GetLoginAsync(unameText.Text, passText.Text).Wait();
+            if (!GetLoginAsync(unameText.Text, passText.Text).Result)
+            {
+                MessageBox.Show("Error. Incorrect Credentials.");
+                this.Close();
+            }
       
             //show next window, pass permissions as level
             //idea modified from https://stackoverflow.com/questions/5548746/c-sharp-open-a-new-form-then-close-the-current-form
